6a25e4a9dd940709a43765379242705278f1ffcc
cfuqua9 <59981807+cfuqua9@users.noreply.github.com>
2020-03-10 16:34:42 -0500

    Update TempCalculationEngine.cs

--- SummarizeTempsObjects/SummarizeTempsObjects/TempCalculationEngine.cs
+++ SummarizeTempsObjects/SummarizeTempsObjects/TempCalculationEngine.cs
@@ -17,8 +17,11 @@ namespace SummarizeTempsObjects
         {
             // add logic to calculate the average
             int sumoftemps = 0;
+
+            // loop through all of the list temps
             foreach(int temp in _temperatures)
             {
+                // add the list item to my summing variable
                 sumoftemps += temp;
             }
             return sumoftemps/ _temperatures.Count; // <-- Replace this with the actual average
@@ -27,13 +30,41 @@ namespace SummarizeTempsObjects
         public int TempsAboveThreshold(int threshold)
         {
             // add logic to calculate temps above the threshold
+
+            int temperatureCount = 0;
+
+            foreach (int temp in _temperatures)
+            {
+
+                if (temp>threshold)
+                {
+                    temperatureCount += 1;
+
+                }
+
+            }
+                
             return 0; // <-- Replace this with the number
         }
 
         public int TempsBelowThreshold(int threshold)
         {
             // add logic to calculate and return temps below the threshold
-            return 0; // <-- Replace this with the number
+
+            int temperatureCount = 0;
+            // loop though all of the list items
+            foreach (int temp in _temperatures)
+            {
+                // if the temp < threshold
+                if (temp < threshold)
+                {
+                    // increment my counter
+                    temperatureCount += 1;
+                }
+
+            }
+
+            return temperatureCount; // <-- Replace this with the number
         }
 
         public int TempsAtThreshold(int threshold)
